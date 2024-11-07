782b7d17ab1b647dae561868973edb2256b880fc
axel-durand <axeldrd.pavil@gmail.com>
2024-11-07 09:35:29 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''module qui permet de déterminer si un nombre est premier ou non'''
 from math import sqrt
 
 #### Fonction secondaire
@@ -6,21 +7,29 @@ from math import sqrt
 def isprime(p):
 
     # votre code ici
-premier = True
-s = int(math.sqrt(p))
-for i in range(2,s+1):
-if p%i == 0:
-    premier=False
-    
-return premier
-    pass
+    '''
+    retourne un booléen, vrai si le nombre passé en paramètre 
+    est premier et faux dans le cas contraire
+    '''
+    premier = True
+    s = int(sqrt(p))
+    if p==1:
+        premier = False
+    for i in range(2,s+1):
+        if p%i == 0:
+            premier=False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
-
+    '''permet de faire des appels de la fonction isprime()'''
     # vos appels à la fonction secondaire ici
+    print(isprime(5))
+    print(isprime(57))
+    print(isprime(95))
 
     for n in range(100):
         if isprime(n):
